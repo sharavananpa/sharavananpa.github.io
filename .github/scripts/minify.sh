403c881ce8e03e0942917f1066aa95fe0ac7f1df
Sharavanan <sharavananpa@gmail.com>
2024-11-10 05:32:35 +0530

    Add stars animation

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -4,6 +4,7 @@ npm install gulp gulp-htmlmin gulp-clean-css gulp-uglify gulp-minify-inline gulp
 gulp
 
 mv CNAME dist/CNAME
+mv noise-transparent.png dist/noise-transparent.png
 rm *
 mv -v ./dist/* .
 rm dist
